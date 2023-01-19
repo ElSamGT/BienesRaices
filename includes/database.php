9dfdf7e27268fa6cb33e770d643ae165ca56c46a
ElSamGT <bikuhan@hotmail.com>
2023-01-19 03:08:02 -0600

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -2,10 +2,10 @@
 
 function conectarDB(): mysqli{
     $db =new  mysqli(
-        $_ENV["DB_HOST"],
-        $_ENV["DB_USER"],
-        $_ENV["DB_PASS"],
-        $_ENV["DB_BD"]
+        "mysql-elsamgt.alwaysdata.net",
+        "elsamgt",
+        "DB_PASSelSamgt",
+        "elsamgt_bienesraices"
     );
 
     $db->set_charset("utf8");
